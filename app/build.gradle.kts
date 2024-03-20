a1845a7a86a3bb59d2e7e93440f9699bd7597453
akshaylalsp <akshaylalsp@gmail.com>
2024-03-20 20:01:30 +0530

    added api service

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -51,8 +53,10 @@ android {
 
 dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:retrofit:convertor-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.7.0")
+//    implementation("com.squareup.retrofit2:retrofit:convertor-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.7.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
