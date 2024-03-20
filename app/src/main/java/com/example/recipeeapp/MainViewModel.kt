3d5503b8662f5be1401017160755f1dddcc87b72
akshaylalsp <akshaylalsp@gmail.com>
2024-03-20 21:41:11 +0530

    implemented view model

---
+++ app/src/main/java/com/example/recipeeapp/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.recipeeapp
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+class MainViewModel : ViewModel() {
+
+    private var _categoriesState = mutableStateOf(RecipeState())
+    val categoriesState : State<RecipeState> = _categoriesState
+    data class RecipeState(
+        val loading:Boolean = true,
+        val list : List<Category> = emptyList(),
+        val error : String? = null
+    )
+    private fun fetchData(){
+        viewModelScope.launch {
+            try {
+                val response = recipeService.getCategories()
+                _categoriesState.value = _categoriesState.value.copy(loading = false, error = null, list = response.categories)
+            }
+            catch (e:Exception){
+                _categoriesState.value = _categoriesState.value.copy(error = "${e.message}", loading = false)
+            }
+        }
+    }
+}
