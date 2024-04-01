a3bd7576e4faaece524c2ed9e8072f1608b17fa5
akshaylalsp <akshaylalsp@gmail.com>
2024-04-01 22:13:53 +0530

    minor changes

--- app/src/main/java/com/example/recipeeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/recipeeapp/RecipeScreen.kt
@@ -1,34 +1,45 @@
 package com.example.recipeeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 
 @Composable
-fun RecipeScreen(modifier: Modifier = Modifier){
-    val recipeViewModel: MainViewModel = viewModel()
-    val viewState by recipeViewModel.categoriesState
+fun RecipeScreen(modifier: Modifier = Modifier,navigateToDetail:(Category)->Unit,viewState:MainViewModel.RecipeState){
     Box(modifier = Modifier.fillMaxSize()) {
         when{
             viewState.loading -> {
@@ -39,27 +50,35 @@ fun RecipeScreen(modifier: Modifier = Modifier){
                 Text(text = "Error occurred")
             }
             else -> {
-                CategoryScreen(categories = viewState.list)
+                CategoryScreen(categories = viewState.list, navigateToDetail = navigateToDetail)
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories:List<Category>){
+fun CategoryScreen(categories:List<Category>,navigateToDetail:(Category)->Unit){
     LazyVerticalGrid(columns = GridCells.Fixed(2)){
         items(categories){
-            category -> ItemView(category = category)
+            category -> ItemView(category = category, navigateToDetail = navigateToDetail)
         }
     }
 }
 
 @Composable
-fun ItemView(category: Category){
-    Column(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = rememberAsyncImagePainter(category.strCategoryThumb), contentDescription = "",Modifier.aspectRatio(1f))
+fun ItemView(category: Category,navigateToDetail:(Category)->Unit){
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .clickable { navigateToDetail(category) }
+        , horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = "",
+            Modifier.aspectRatio(1f)
+        )
         Text(text = category.strCategory)
     }
 }
